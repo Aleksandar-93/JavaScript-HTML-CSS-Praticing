408b3e305dcf4861db01481c1167887241736bd4
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-02-04 00:19:01 +0100

    close other on click

--- 7-questions/app.js
+++ 7-questions/app.js
@@ -1,8 +1,19 @@
 //using selectors inside the element
 
+ const questions = document.querySelectorAll('.question');
 
+ questions.forEach(el => {
+        const btn = el.querySelector('.question-btn');
+        btn.addEventListener('click', ()=>{
+            questions.forEach(item => {
+                if(item !== el){
+                    item.classList.remove('show-text')
+                }
+            });
 
-
+            el.classList.toggle('show-text')
+        })
+    });
 
 
 
@@ -12,11 +23,11 @@
 
 // traversing the dom
 
-const btns = document.querySelectorAll('.question-btn');
-btns.forEach(element => {
-  element.addEventListener('click', (e) => {
-    const question = e.currentTarget.parentElement.parentElement;
-    question.classList.toggle('show-text');
-  });
-});
+// const btns = document.querySelectorAll('.question-btn');
+// btns.forEach(element => {
+//   element.addEventListener('click', (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   });
+// });
 
