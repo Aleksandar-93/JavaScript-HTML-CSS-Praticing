dcb7b1151bc528443e1c434b030b7007746034d5
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-02-03 22:19:56 +0100

    adding js open/close Model btn

--- 6-modal/app.js
+++ 6-modal/app.js
@@ -1,4 +1,16 @@
 // select modal-btn,modal-overlay,close-btn
+const openModelBtn = document.querySelector('.modal-btn');
+const modelOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
 // listen for click events on modal-btn and close-btn
+
 // when user clicks modal-btn add .open-modal to modal-overlay
+openModelBtn.addEventListener('click', () =>{
+    modelOverlay.classList.add('open-modal');
+});
+
 // when user clicks close-btn remove .open-modal from modal-overlay
+closeBtn.addEventListener('click', () =>{
+    modelOverlay.classList.remove('open-modal');
+});
