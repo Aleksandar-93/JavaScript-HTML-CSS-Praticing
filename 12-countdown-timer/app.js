8862e3a75942bbe9939e0e0eb76e54ea61f868ba
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-02-08 22:33:12 +0100

    coutdown always 10 days left

--- 12-countdown-timer/app.js
+++ 12-countdown-timer/app.js
@@ -26,8 +26,17 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMounth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
 // set giveaway date
-let futureDate = new Date(2021,3,8,11,30,0);
+
+
+// let futureDate = new Date(2021, 3, 8, 11, 30, 0);
+
+const futureDate = new Date(tempYear, tempMounth, tempDay + 10, 11, 30, 0)
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
