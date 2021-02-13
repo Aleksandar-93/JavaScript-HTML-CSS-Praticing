5df02879a3c590f97ba316eda9c6f771b10cc7ba
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-02-13 20:39:06 +0100

    back to default/clear input

--- 14-grocery-bud/app.js
+++ 14-grocery-bud/app.js
@@ -47,6 +47,11 @@ function addItem(e){
         displayAlert('item added to the list', 'success')
         // show container
         container.classList.add('show-container')
+        // add to local storage
+        addToLocalStorage(id, value);
+        // set back to default
+        setBackToDefault();
+
     }else if (value !== '' && editFlag === true){
         console.log("editing");
     }else{
@@ -65,7 +70,16 @@ function displayAlert(text, action){
     alert.classList.remove(`alert-${action}`);
     }, 1000)
 }
+// set back to default
+function setBackToDefault(){
+    grocery.value = '';
+    editFlag = false;
+    editID = '';
+    submitBtn.textContent = 'submit';
+}
 // ****** LOCAL STORAGE **********
-
+function addToLocalStorage(id, value){
+    console.log('added to local storage')
+}
 // ****** SETUP ITEMS **********
  
