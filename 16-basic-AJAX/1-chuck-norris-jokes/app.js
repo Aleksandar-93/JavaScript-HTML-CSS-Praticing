35c53fa6785cda2b15f8081116c5660aa6e65d1a
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-04-13 21:59:35 +0200

    promisify my js

--- 16-basic-AJAX/1-chuck-norris-jokes/app.js
+++ 16-basic-AJAX/1-chuck-norris-jokes/app.js
@@ -5,29 +5,37 @@ const img = document.querySelector('.container img')
 const url = 'https://api.chucknorris.io/jokes/random';
 
 btn.addEventListener('click', ()=>{
-    getData(url);
+    getData(url)
+    .then(response => displayData(response))
+    .catch(err => console.log(err));
 });
 
 function getData(url){
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send();
-    xhr.onreadystatechange = function (){
+    return new Promise((resolve,reject)=>{
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.send();
+        xhr.onreadystatechange = function (){
         if(xhr.readyState !==4) return;
         if(xhr.status === 200){
-            img.classList.add('shake-img');
-            const {value:joke} = JSON.parse(xhr.responseText);
-            content.textContent = joke;
-            const random = Math.random() * 1000;
-            setTimeout(()=>{ 
-            img.classList.remove('shake-img')
-             }, random);
+            resolve(xhr.responseText)
         }else{
-            console.log({
+            reject({
                 status: xhr.status,
                 text: xhr.statusText,
-            });
+            });  
         }
     }
+    })
+    
 }
 
+function displayData(data){
+    img.classList.add('shake-img');
+    const {value:joke} = JSON.parse(data);
+    content.textContent = joke;
+    const random = Math.random() * 1000;
+    setTimeout(()=>{ 
+        img.classList.remove('shake-img')
+    }, random);
+}
