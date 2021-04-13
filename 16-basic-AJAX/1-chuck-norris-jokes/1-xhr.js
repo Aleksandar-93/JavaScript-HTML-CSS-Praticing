9262d56f7793311719442a4f026ef04980657030
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-04-13 21:36:37 +0200

    shake animation

--- 16-basic-AJAX/1-chuck-norris-jokes/1-xhr.js
+++ 16-basic-AJAX/1-chuck-norris-jokes/1-xhr.js
@@ -1,6 +1,5 @@
 const btn = document.querySelector('.btn');
 const content = document.querySelector('.content');
-const img = document.querySelector('.container img')
 
 const url = 'https://api.chucknorris.io/jokes/random';
 
@@ -15,13 +14,8 @@ function getData(url){
     xhr.onreadystatechange = function (){
         if(xhr.readyState !==4) return;
         if(xhr.status === 200){
-            img.classList.add('shake-img');
             const {value:joke} = JSON.parse(xhr.responseText);
             content.textContent = joke;
-            const random = Math.random() * 1000;
-            setTimeout(()=>{ 
-            img.classList.remove('shake-img')
-             }, random);
         }else{
             console.log({
                 status: xhr.status,
