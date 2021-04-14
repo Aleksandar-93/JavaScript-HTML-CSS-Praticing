be32dcbeeb3795167dd1cc0e07d0996cd2dd2983
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-04-14 21:47:13 +0200

    get elements

--- 16-basic-AJAX/2-random-user/app.js
+++ 16-basic-AJAX/2-random-user/app.js
@@ -1,1 +1,13 @@
- 
+import get from './utils/getElement.js';
+
+const url = 'https://randomuser.me/api/';
+
+ const img = get('.user-img');
+ const title = get('.user-title');
+ const value = get('.user-value');
+ const btn = get('.btn');
+ const btns = [...document.querySelectorAll('.icon')];
+
+
+
+
