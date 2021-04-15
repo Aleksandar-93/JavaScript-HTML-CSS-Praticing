91410b6c54d3070ad8f015689e61f0714c0655e8
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-04-15 17:02:22 +0200

    get user

--- 16-basic-AJAX/2-random-user/app.js
+++ 16-basic-AJAX/2-random-user/app.js
@@ -1,13 +1,20 @@
 import get from './utils/getElement.js';
+import getUser from './utils/fetchUser.js'
 
-const url = 'https://randomuser.me/api/';
+const img = get('.user-img');
+const title = get('.user-title');
+const value = get('.user-value');
+const btn = get('.btn');
+const btns = [...document.querySelectorAll('.icon')];
 
- const img = get('.user-img');
- const title = get('.user-title');
- const value = get('.user-value');
- const btn = get('.btn');
- const btns = [...document.querySelectorAll('.icon')];
+const showUser = async () =>{
+    //get user from api
+    const person = await getUser();
+    console.log(person);
+    //display user
+};
 
-
+window.addEventListener('DOMContentLoaded', showUser);
+btn.addEventListener('click', showUser);
 
 
