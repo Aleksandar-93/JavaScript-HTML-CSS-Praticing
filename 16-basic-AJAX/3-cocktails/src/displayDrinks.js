51c6e01d4d567da15dc2cff45c17b180e5b2f478
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-04-20 20:48:33 +0200

    toggle loading

--- 16-basic-AJAX/3-cocktails/src/displayDrinks.js
+++ 16-basic-AJAX/3-cocktails/src/displayDrinks.js
@@ -1,10 +1,12 @@
 import get from './getElement.js'
+import {hideLoading} from './toggleLoading.js'
 
 const displayDrinks = ({drinks})=>{
     const section = get('.section-center');
     const title = get('.title');
     if(!drinks){
         // hide loading
+        hideLoading();
         title.textContent = 'Sorry, no drinks matched your search';
         section.innerHTML = null;
         return;
@@ -21,6 +23,7 @@ const displayDrinks = ({drinks})=>{
         `
     }).join('');
     // hide loading
+    hideLoading();
     title.textContent = '';
     section.innerHTML = newDrinks;
     return section;
