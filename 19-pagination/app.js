2004d85994c6d6e7661a6ea09e3c72c6a846d8ad
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-05-16 16:29:25 +0200

    create pages

--- 19-pagination/app.js
+++ 19-pagination/app.js
@@ -8,7 +8,9 @@ const title = document.querySelector('.section-title h1')
 
 const init = async ()=>{
     const followers = await fetchFollowers();
-    displayFollowers(followers);
+    displayFollowers(paginate(followers)[0]);
     title.textContent = 'pagination';
+    const pages = paginate(followers);
+    console.log(pages);
 }
 window.addEventListener('load', init);
